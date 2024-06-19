7984c0426b9f07011d10b4359313b01d4414812b
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-19 23:09:50 +0530

    Initial Commit

---
+++ Example-Observable-iOS17/Example_Observable_iOS17App.swift
@@ -0,0 +1,17 @@
+//
+//  Example_Observable_iOS17App.swift
+//  Example-Observable-iOS17
+//
+//  Created by Mayur Vaity on 19/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Example_Observable_iOS17App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
