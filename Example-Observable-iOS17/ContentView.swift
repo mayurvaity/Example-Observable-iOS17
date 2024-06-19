f9dc12a300c5b88358301f6a3a05d79946a3a4f5
Mayur Vaity <mayur.m.vaity@icloud.com>
2024-06-19 23:17:52 +0530

    Old way of ObservableObject implementation.

--- Example-Observable-iOS17/ContentView.swift
+++ Example-Observable-iOS17/ContentView.swift
@@ -7,13 +7,44 @@
 
 import SwiftUI
 
+class User: ObservableObject {
+    @Published var name: String
+    @Published var jobTitle: String
+    @Published var followerCount: Int
+    @Published var bio: String
+    
+    init(name: String, jobTitle: String, followerCount: Int, bio: String) {
+        self.name = name
+        self.jobTitle = jobTitle
+        self.followerCount = followerCount
+        self.bio = bio
+    }
+}
+
 struct ContentView: View {
+    
+    @StateObject var user = User(name: "Mayur Vaity",
+                                 jobTitle: "iOS Dev",
+                                 followerCount: 99,
+                                 bio: "My bio goes here")
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(alignment: .leading) {
+            Text(user.name)
+                .font(.title.bold())
+            Text(user.jobTitle)
+                .foregroundStyle(.secondary)
+            
+            Text("\(user.followerCount) Followers")
+                .foregroundStyle(.secondary)
+            
+            Button {
+                user.followerCount += 1
+            } label: {
+                Text("Add follower")
+            }
+            .buttonStyle(.borderedProminent)
+            .padding(.top, 30)
         }
         .padding()
     }
